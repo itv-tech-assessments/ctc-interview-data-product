d47c99ad021fb3d361596ddf99facb3ee6b607a5
samuhepp <61977352+samuhepp@users.noreply.github.com>
2025-04-07 09:56:17 +0100

    Add join to filter ad consent

--- data-transformation/app.py
+++ data-transformation/app.py
@@ -14,12 +14,12 @@ def main():
 
 def load_raw(spark: SparkSession):
     # process user data first
-    df = spark.read.json("./user_test_file.json", multiLine=True)
-    df.filter(col("username").isNotNull()).withColumn("dob", when(col("dob") == lit(""), lit(None).cast("string")).otherwise(col("dob"))).write.format("delta").mode("overwrite").save("./out/user")
+    df1 = spark.read.json("./user_test_file.json", multiLine=True)
+    df1.filter(col("username").isNotNull()).withColumn("dob", when(col("dob") == lit(""), lit(None).cast("string")).otherwise(col("dob"))).write.format("delta").mode("overwrite").save("./out/user")
 
-    df = spark.read.csv("./ads_test_file.csv",header=True,schema="event_type string, timestamp string, user string, content string, ad string")
-    df = df.filter(col("event_type")=="ad_completed").groupBy("user").count()
-    df.write.format("delta").mode("overwrite").save("./out/user_ad_count")
+    df2 = spark.read.csv("./ads_test_file.csv",header=True,schema="event_type string, timestamp string, user string, content string, ad string")
+    df2 = df2.filter(col("event_type")=="ad_completed").join(df1, df1.username == df2.user).groupBy("user").count()
+    df2.write.format("delta").mode("overwrite").save("./out/user_ad_count")
 
 
 
